fb054d551b2107768cf3203e5bb3a3d20c28c4fc
Haoran Wang <b50027@freescale.com>
2015-05-29 10:08:13 +0800

    MA-6749 Do not find slotId when check Ethernet in DataUsage
    As the slotId check in DataUsage doesn't consider the
    Ethernet tag and it will cause Settings crash. So ignore
    the slotId check when check Ethernet DataUsage.
    
    Signed-off-by: Haoran Wang <b50027@freescale.com>

--- src/com/android/settings/DataUsageSummary.java
+++ src/com/android/settings/DataUsageSummary.java
@@ -832,7 +832,7 @@ public class DataUsageSummary extends HighlightingFragment implements Indexable 
         mBinding = false;
 
         int seriesColor = resources.getColor(R.color.sim_noitification);
-        if (mCurrentTab != null && mCurrentTab.length() > TAB_MOBILE.length() ){
+        if (mCurrentTab != null && mCurrentTab.length() > TAB_MOBILE.length() && !TAB_ETHERNET.equals(currentTab)){
             final int slotId = Integer.parseInt(mCurrentTab.substring(TAB_MOBILE.length(),
                     mCurrentTab.length()));
             final SubscriptionInfo sir = com.android.settings.Utils.findRecordBySlotId(context,
